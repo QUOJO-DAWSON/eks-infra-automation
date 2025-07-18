d8df0d3cfd49ba9cec125039924237bf73f21444
QUOJO-DAWSON <dawsonkessongp@gmail.com>
2025-07-18 17:41:11 +0000

    chore: trigger ci

--- istio.tf
+++ istio.tf
@@ -9,13 +9,3 @@ resource "helm_release" "istio-base" {
   depends_on       = [module.eks, helm_release.aws-load-balancer-controller, helm_release.external-secrets]
 }
 
-# 2. Install istiod second (control plane)
-resource "helm_release" "istiod" {
-  name             = "istiod"
-  repository       = "https://istio-release.storage.googleapis.com/charts"
-  chart            = "istiod"
-  version          = "1.26.2"
-  create_namespace = false
-  namespace        = "istio-system"
-  depends_on       = [helm_release.istio-base]
-}
