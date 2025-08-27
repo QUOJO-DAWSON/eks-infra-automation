0d69c0bfbf9e6866ba6e4936e7a035a45cdfb79c
iamfet <fetuyee@gmail.com>
2025-08-27 17:40:31 +0000

    refactor: update helm chart version

--- prometheus.tf
+++ prometheus.tf
@@ -2,7 +2,7 @@ resource "helm_release" "istio_prometheus" {
   name             = "kube-prometheus-stack"
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "kube-prometheus-stack"
-  version          = "75.12.0"
+  version          = "76.4.0"
   create_namespace = true
   namespace        = "monitoring"
   depends_on       = [helm_release.istiod]
